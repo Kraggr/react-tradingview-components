2c08bf8d30a52218fc5ea1ff0e13120832d3cec0
Hugo Jimenez <hugoduar1@gmail.com>
2021-04-12 03:16:35 -0500

    Updates

---
+++ precache-manifest.4ac13d40219745c756c90bfd58356bdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3888e32d728fd9f7d4a0728c2228aff5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa2a1f779c75daedb90b",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "f5431726cf611c0b2e44",
+    "url": "./static/js/2.8c12e685.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8c12e685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2a1f779c75daedb90b",
+    "url": "./static/js/main.aeab21e0.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.aeab21e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d8d8d679dde7f2915d",
+    "url": "./static/js/runtime-main.05195c27.js"
+  }
+]);
